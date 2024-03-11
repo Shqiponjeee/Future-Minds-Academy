e277f0cb2431dcb193108a797c439529db611d07
Shqiponjeee <vitijashqiponje@gmail.com>
2024-03-11 21:50:00 +0100

    j

--- Projects/JavaScript Final Project/js/listings.js
+++ Projects/JavaScript Final Project/js/listings.js
@@ -1,68 +1,79 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    let listings = JSON.parse(localStorage.getItem('listings')) || [
-        {
-            id: 1,
-            name: "Beautiful Castle",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
-            price: 899,
-            currency: '$',
-            location: {
-                city: 'Barcelona',
-                country: 'Spain'
+    let isPageReloaded = false; 
+
+    let listings = JSON.parse(localStorage.getItem('listings')) || getDefaultListings();
+
+    function getDefaultListings() {
+        return [
+            {
+                id: 1,
+                name: "Beautiful Castle",
+                description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
+                price: 899,
+                currency: '$',
+                location: {
+                    city: 'Barcelona',
+                    country: 'Spain'
+                },
+                image: 'images/card-1.jpeg'
             },
-            image: 'images/card-1.jpeg'
-        },
-        {
-            id: 2,
-            name: "Office Studio",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
-            price: 1119,
-            currency: '$',
-            location: {
-                city: 'London',
-                country: 'UK'
+            {
+                id: 2,
+                name: "Office Studio",
+                description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
+                price: 1119,
+                currency: '$',
+                location: {
+                    city: 'London',
+                    country: 'UK'
+                },
+                image: 'images/card-2.jpeg'
             },
-            image: 'images/card-2.jpeg'
-        },
-        {
-            id: 3,
-            name: "Cozy 5 Stars Apartment",
-            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
-            Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
-            price: 459,
-            currency: '$',
-            location: {
-                city: 'Rome',
-                country: 'Italy'
+            {
+                id: 3,
+                name: "Cozy 5 Stars Apartment",
+                description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.
+                Vivamus id rutrum mauris. Praesent hendrerit accumsan viverra.`,
+                price: 459,
+                currency: '$',
+                location: {
+                    city: 'Rome',
+                    country: 'Italy'
+                },
+                image: 'images/card-3.jpeg'
             },
-            image: 'images/card-3.jpeg'
-        },
-    ];
+        ];
+    }
 
     function saveListingsToLocalStorage() {
         localStorage.setItem('listings', JSON.stringify(listings));
     }
 
     function renderListings() {
-        let cardsContainer = document.querySelector('.cards-container');
+        let cardsContainer = document.querySelector('.cards');
         cardsContainer.innerHTML = '';
+
+        if (!isPageReloaded && listings.length === 0) {
+            listings = getDefaultListings();
+            saveListingsToLocalStorage();
+        }
 
         listings.forEach(listing => {
             let cardWrapper = document.createElement('div');
-            cardWrapper.classList.add('card');
+            cardWrapper.classList.add('card-wraper');
             cardWrapper.dataset.id = listing.id;
 
             let card = document.createElement('div');
             card.classList.add('card');
 
             let cardHovered = document.createElement('div');
-            cardHovered.classList.add('image-card');
+            cardHovered.classList.add('card-hovered');
 
             let imgHover = document.createElement('div');
             imgHover.classList.add('img-hover');
@@ -79,10 +90,8 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
             let editButton = createIconButton('edit', 'Edit', function (event) {
-                let cardWrapper = event.target.closest('.card');
-
-
-                let editableElements = cardWrapper.querySelectorAll('.title, .paragraph, .price-card, .location-card');
+                let cardWrapper = event.target.closest('.card-wraper');
+                let editableElements = cardWrapper.querySelectorAll('.card-title, .card-paragraf, .price, .location');
 
                 if (editableElements.length > 0) {
                     let firstEditableElement = editableElements[0];
@@ -92,22 +101,21 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
             let deleteButton = createIconButton('close', 'Delete', function (event) {
-                let cardWrapper = event.target.closest('.card');
+                let cardWrapper = event.target.closest('.card-wraper');
                 let id = parseInt(cardWrapper.dataset.id);
-                let listingIndex = listings.findIndex(item => item.id === id);
+                let listing = listings.find(item => item.id === id);
 
-                let confirmDelete = confirm("Are you sure you want to delete this listing?");
+                let confirmDelete = confirm;
                 if (confirmDelete) {
                     cardWrapper.classList.add('delete-animation');
                     setTimeout(function () {
                         cardWrapper.remove();
-                        listings.splice(listingIndex, 1);
+                        listings = listings.filter(item => item.id !== listing.id);
                         saveListingsToLocalStorage();
-                    }, 500);
+                        renderListings();
+                    }, 500); 
                 }
             });
-
-
 
             cardIcons.appendChild(viewButton);
             cardIcons.appendChild(editButton);
@@ -121,14 +129,14 @@ document.addEventListener('DOMContentLoaded', function () {
             let cardInfo = document.createElement('div');
             cardInfo.classList.add('card-info');
 
-            let cardTitleSpan = createEditableElement('span', 'title', listing.name);
-            let cardParagraf = createEditableElement('span', 'paragraph', listing.description);
+            let cardTitleSpan = createEditableElement('span', 'card-title', listing.name);
+            let cardParagraf = createEditableElement('span', 'card-paragraf', listing.description);
             let separator = document.createElement('div');
-            separator.classList.add('hr1');
+            separator.classList.add('separator');
             let bottomContent = document.createElement('div');
             bottomContent.classList.add('bottom-content');
-            let priceSpan = createEditableElement('div', 'price-card', `${listing.price}${listing.currency}/night`);
-            let locationSpan = createEditableElement('div', 'location-card', `${listing.location.city}, ${listing.location.country}`);
+            let priceSpan = createEditableElement('div', 'price', `${listing.price}${listing.currency}/night`);
+            let locationSpan = createEditableElement('div', 'location', `${listing.location.city}, ${listing.location.country}`);
 
             cardInfo.appendChild(cardTitleSpan);
             cardInfo.appendChild(cardParagraf);
@@ -142,6 +150,8 @@ document.addEventListener('DOMContentLoaded', function () {
             cardWrapper.appendChild(card);
             cardsContainer.appendChild(cardWrapper);
         });
+
+        isPageReloaded = true;
     }
 
     function createEditableElement(tagName, className, textContent) {
@@ -163,26 +173,27 @@ document.addEventListener('DOMContentLoaded', function () {
             element.contentEditable = false;
             let newText = element.innerHTML.trim();
             if (newText !== '') {
-                let cardWrapper = element.closest('.card');
+                let cardWrapper = element.closest('.card-wraper');
                 let id = parseInt(cardWrapper.dataset.id);
                 let listing = listings.find(item => item.id === id);
 
-                if (element.classList.contains('title')) {
+                if (element.classList.contains('card-title')) {
                     listing.name = newText;
-                } else if (element.classList.contains('paragraph')) {
+                } else if (element.classList.contains('card-paragraf')) {
                     listing.description = newText;
-                } else if (element.classList.contains('price-card')) {
+                } else if (element.classList.contains('price')) {
                     let newPrice = parseFloat(newText);
                     if (!isNaN(newPrice)) {
                         listing.price = newPrice;
                     }
-                } else if (element.classList.contains('location-card')) {
+                } else if (element.classList.contains('location')) {
                     let [newCity, newCountry] = newText.split(',').map(item => item.trim());
                     listing.location.city = newCity;
                     listing.location.country = newCountry;
                 }
 
                 saveListingsToLocalStorage();
+                renderListings();
             }
         });
 
@@ -193,20 +204,19 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-
     function createIconButton(iconName, tooltip, clickHandler) {
         let button = document.createElement('span');
         button.classList.add('header-icon', 'tooltip-hover');
-
-
+        button.title = tooltip;
+        button.dataset.tooltip = tooltip;
         button.innerHTML = `<span class="material-icons">${iconName}</span>`;
 
         if (iconName === 'burst_mode') {
-            button.classList.add('view');
+            button.classList.add('view-icon');
         } else if (iconName === 'edit') {
-            button.classList.add('edit');
+            button.classList.add('edit-icon');
         } else if (iconName === 'close') {
-            button.classList.add('delete');
+            button.classList.add('delete-icon');
         }
 
         if (clickHandler) {
